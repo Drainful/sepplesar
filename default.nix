a2f1f6e37fd66d6dbb7eb9818e82d6cc2ab6012f
Adrian Fullmer <ipkcle@gmail.com>
2019-03-02 15:54:19 -0500

    Fix default.nix

--- default.nix
+++ default.nix
@@ -3,14 +3,17 @@ unstable = import (fetchTarball https://nixos.org/channels/nixos-unstable/nixexp
 in
 with import <nixpkgs> {};
 let
-	opencvGtk = opencv.override (old : { enableGtk2 = true; });
-	opencv4-override = unstable.opencv4.override (old : { enableGtk3 = true; enableDocs = true; enableCuda = true; enableFfmpeg = true; });
+	opencvGtk = opencv.override
+	(old : { enableGtk2 = true; });
+	opencv4-override = unstable.opencv4.override
+	(old : { enableGtk3 = true; enableDocs = true; enableCuda = true; enableFfmpeg = true; });
 in {
   surfaces = stdenv.mkDerivation rec {
     name = "sepplesar";
     buildInputs = [
       clang_6
 
+			opencvGtk
 			opencv4-override
 
       pkgconfig
